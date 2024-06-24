fa11fab002c2bb7aa3bd2ef63210d8926613afd4
seongjun-noh <seongjun5657@gmail.com>
2024-06-24 20:35:00 +0900

    init

---
+++ src/main/java/com/flog/FlogApplication.java
@@ -0,0 +1,13 @@
+package com.flog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flog/FlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
